1130d2d0c9409309b7d08e34cb442bf2ab6f111a
Dat Nguyen <dat.nguyen@ge.com>
2015-05-26 23:28:28 -0700

    updating query

--- src/main/java/org/dtn82/BSRInformationRepository.java
+++ src/main/java/org/dtn82/BSRInformationRepository.java
@@ -12,9 +12,20 @@ import java.util.List;
 
 public interface BSRInformationRepository extends MongoRepository<BSRInformation, String> {
 
-    public BSRInformation findByDate(Date date);
-
+//    public BSRInformation findByDate(Date date);
+////
     @Query("{ 'tokens' : { $in : ?0 } }")
     public List<BSRInformation> findByTokens(String[] tokens);
+////
+////    @Query("{ 'date' : { '$gte' : ?0 }, 'date' : { '$lte' : ?1 } }")
+////    public List<BSRInformation> findByDateRange(Date start, Date End);
 
+//    {"tokens": {"$in":["bill"]},"date": {"$gte": "2015-01-01","$lte": "2015-05-01"}}
+
+
+    @Query("{ 'info' : { $regex : ?0 } }")
+    public List<BSRInformation> findByInfo(String pattern);
+
+    @Query("{ 'info' : { $regex : ?0 }, 'date' : { '$gte' : ?1, '$lte' : ?2 } }")
+    public List<BSRInformation> get(String pattern, Date start, Date End);
 }
--- src/main/java/org/dtn82/ResultsController.java
+++ src/main/java/org/dtn82/ResultsController.java
@@ -7,6 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 @RestController
@@ -16,6 +20,7 @@ public class ResultsController {
 
     @Autowired
     BSRInformationRepository bsrInformationRepository;
+
 
 
 //    @RequestMapping(value = "/", method = RequestMethod.GET)
@@ -34,14 +39,63 @@ public class ResultsController {
     }
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
-    public ResponseEntity<List<BSRInformation>> get(@RequestParam(value = "keys") String keys)
+    public ResponseEntity<List<BSRInformation>> get(@RequestParam(value = "keys", required = false) String keys, @RequestParam(value = "start_date", required = false) String start, @RequestParam(value = "end_date", required = false) String end)
     {
-        keys = keys.toLowerCase();
-        keys = keys.replaceAll("\\s+", "");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-M-dd");
+        String tokens = null;
+        Date start_date = null;
+        Date end_date = null;
 
-        String[] tokens = keys.split(",");
+        if (keys != null)
+        {
+            keys = keys.toLowerCase();
+            keys = keys.replaceAll("\\s+", "");
+            tokens = keys.replaceAll(",", "|");
+        }
+        else
+        {
+            tokens = new String("");
+        }
 
-        List<BSRInformation> list =  bsrInformationRepository.findByTokens(tokens);
+
+        if (start == null)
+        {
+            start_date = new GregorianCalendar(2007, 6, 14).getTime();
+        }
+        else
+        {
+            try {
+                start_date = dateFormat.parse(start);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (end == null)
+        {
+            end_date = new GregorianCalendar(2015, 5, 8).getTime();
+        }
+        else
+        {
+            try {
+                end_date = dateFormat.parse(end);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(start_date.toString());
+
+        System.out.println(end_date.toString());
+
+        List<BSRInformation> list = bsrInformationRepository.get(tokens, start_date, end_date);
+
+
+        for (BSRInformation l : list)
+        {
+            System.out.println(l.toString()) ;
+        }
+
         return new ResponseEntity<List<BSRInformation>>(list, new HttpHeaders(), HttpStatus.OK);
     }
 
